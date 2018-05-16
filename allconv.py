2509c19489bea8cdc5aecc4d7e546e99348a8353
JonasWechsler <jonaswechsler@gmail.com>
2018-05-16 10:41:55 +0200

    Refactored allconv.py to have a global settings class

--- allconv.py
+++ allconv.py
@@ -1,7 +1,6 @@
 from __future__ import print_function
 import tensorflow as tf
 from keras.datasets import cifar10
-#from keras.preprocessing.image import ImageDataGenerator
 import image
 from image import ImageDataGenerator
 from keras.models import Sequential
@@ -20,10 +19,54 @@ import os
 import pandas
 import numpy as np
 
-def make_model(weights=None, lr=0.01, decay=1e-7):
-    model = Sequential()
+K.set_image_dim_ordering('tf')
+
+def load_data():
+    (X_train, y_train), (X_test, y_test) = cifar10.load_data()
+    n_classes = len(set(y_train.flatten()))
+
+    Y_train = np_utils.to_categorical(y_train, n_classes) # Convert to one-hot vector
+    Y_test = np_utils.to_categorical(y_test, n_classes)
+
+    X_train = X_train.astype('float32')
+    X_test = X_test.astype('float32')
+
+    X_train /= 255 #Normalize
+    X_test /= 255
+
+    return (X_train, Y_train, X_test, Y_test)
+
+
+def preprocess_dataset(X,Y, settings):
+    if settings.augment_data:
+        datagen = ImageDataGenerator(
+                contrast_stretching=True, adaptive_equalization=False, histogram_equalization=False,
+                #featurewise_center=False,  # set input mean to 0 over the dataset
+                samplewise_center=False,  # set each sample mean to 0
+                featurewise_std_normalization=False,  # divide inputs by std of the dataset
+                samplewise_std_normalization=False,  # divide each input by its std
+                zca_whitening=False,  # whitening
+                rotation_range=0,  # randomly rotate images in the range (degrees, 0 to 180)
+                width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
+                height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
+                horizontal_flip=True,  # randomly flip images
+                vertical_flip=False) 
+    else:
+        datagen = ImageDataGenerator()
     
-    model.add(Conv2D(96, (3, 3), padding = 'same', input_shape=(32, 32, 3)))
+    datagen.fit(X)
+    batches = datagen.flow(X, Y, batch_size=settings.batch_size)
+    
+    return batches, datagen
+
+def make_model(settings, X_train=None):
+    model = Sequential()
+
+    if settings.input_dropout:
+        model.add(Dropout(0.2, input_shape=(32, 32, 3)))
+        model.add(Conv2D(96, (3, 3), padding = 'same'))
+    else:
+        model.add(Conv2D(96, (3, 3), padding = 'same', input_shape=(32, 32, 3)))
     model.add(Activation('relu'))
     model.add(Conv2D(96, (3, 3),padding='same'))
     model.add(Activation('relu'))
@@ -46,152 +89,110 @@ def make_model(weights=None, lr=0.01, decay=1e-7):
     model.add(GlobalAveragePooling2D())
     model.add(Activation('softmax'))
     sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
-    sgd = SGD(lr=lr, decay=decay, momentum=0.9)
+    sgd = SGD(lr=settings.learning_rates[0], decay=settings.decay, momentum=0.9)
 
-    #if os.path.isfile(weights):
-    #    print("loading weights from checkpoint")
-    #    model.load_weights(weights)
-    
-    #batch_size = 32
-    #model = LSUVinit(model, X_train[:batch_size,:,:,:])
+    if settings.weights_path != None and os.path.isfile(weights):
+        print("loading weights from checkpoint")
+        model.load_weights(weights)
+
+    if settings.orthonormal_init: 
+        model = LSUVinit(model, X_train[:settings.batch_size,:,:,:])
+
     model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
     return model
 
-def load_data():
-    (X_train, y_train), (X_test, y_test) = cifar10.load_data()
-    n_classes = len(set(y_train.flatten()))
-
-    Y_train = np_utils.to_categorical(y_train, n_classes) # Convert to one-hot vector
-    Y_test = np_utils.to_categorical(y_test, n_classes)
-
-    X_train = X_train.astype('float32')
-    X_test = X_test.astype('float32')
-
-    X_train /= 255 #Normalize
-    X_test /= 255
-
-    return (X_train, Y_train, X_test, Y_test)
-
-def preprocess_data(X, datagen=None):
-    if datagen  == None:
-        datagen = ImageDataGenerator(zca_whitening=True)
-        datagen.fit(X)
-    for idx in range(X.shape[0]):
-        x = X[idx]
-        p2, p98 = np.percentile(x, (2, 98))
-        x = exposure.rescale_intensity(x, in_range=(p2, p98))
-        x = datagen.standardize(x)
-        X[idx] = x
-    return datagen
-        
-
-def preprocess_dataset(X,Y,batches):
-    datagen = ImageDataGenerator(
-            contrast_stretching=True, adaptive_equalization=False, histogram_equalization=False,
-            #featurewise_center=False,  # set input mean to 0 over the dataset
-            samplewise_center=False,  # set each sample mean to 0
-            featurewise_std_normalization=False,  # divide inputs by std of the dataset
-            samplewise_std_normalization=False,  # divide each input by its std
-            zca_whitening=False,  # whitening
-            rotation_range=0,  # randomly rotate images in the range (degrees, 0 to 180)
-            width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
-            height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
-            horizontal_flip=True,  # randomly flip images
-            vertical_flip=False) 
-    
-    datagen.fit(X)
-    batches = datagen.flow(X, Y, batch_size=batch_size)
-    
-    return batches, datagen
-
-def run(model, epoch_lengths, learning_rates, batches, test_batches):
-    checkpoint = ModelCheckpoint(path, monitor='val_acc', verbose=1, save_best_only=True, save_weights_only=False, mode='max')
+def run(settings, batches, test_batches, X_train):
+    model = make_model(settings, X_train)
+    checkpoint = ModelCheckpoint(settings.weights_path, monitor='val_acc', verbose=1, save_best_only=True, save_weights_only=False, mode='max')
     history = {'val_loss': [], 'val_acc': [], 'loss': [], 'acc': []}
     
     total_epochs = 0
     iter=0
-    for rate, epochs in zip(learning_rates, epoch_lengths):
+    for rate, epochs in zip(settings.learning_rates, settings.epoch_lengths):
         K.set_value(model.optimizer.lr, rate)
-        print(X_train.shape[0], batch_size, X_train.shape[0]//batch_size, len(batches), len(test_batches))
         history_callback = model.fit_generator(batches,
             #steps_per_epoch=X_train.shape[0]/batch_size,
             epochs=epochs,
-            validation_data=(X_test, Y_test),
+            validation_data=test_batches,
             validation_steps=1,
             #validation_steps=X_test.shape[0],
             callbacks=[checkpoint],
             verbose=2)
-        total_epochs += epochs
         next_hist = history_callback.history
         history = {key:history[key] + next_hist[key] for key in history}
         pandas.DataFrame(history).to_csv("history-{}.csv".format(iter))
         iter=iter+1
+        total_epochs += epochs
         for key in history:
             assert(len(history[key]) == total_epochs)
 
     return history
 
-def param_search(decays, learning_rates):
+class Settings:
+    def __init__(self,
+            batch_size=64,
+            epoch_lengths=[100],
+            learning_rates=[0.01],
+            momentum=0.9,
+            weights_path=None,
+            decay=3e-5,
+            input_dropout=False,
+            orthonormal_init=True,
+            augment_data=True):
+        self.batch_size = batch_size
+        self.epoch_lengths = epoch_lengths
+        self.learning_rates = learning_rates
+        self.weights_path=None
+        self.decay = decay
+        self.input_dropout = input_dropout 
+        self.momentum = momentum
+        self.orthonormal_init = orthonormal_init
+        self.augment_data = augment_data
+
+def param_search(settings_list, batches, test_batches, X_train):
     with open('out.txt', 'a') as f:
-        for decay, lr in zip(decays, learning_rates):
-            learning_rate = 0.012
-            epochs = 20
-            model = make_model(path, learning_rate, decay)
-            print(model.summary())
-            history = run(model, [epochs], [learning_rate], batches, test_batches)
+        for settings in settings_list:
+            history = run(settings, batches, test_batches, X_train)
+            pandas.DataFrame(history).to_csv("history-{}-{}.csv".format(settings.decay,settings.learning_rate[0]))
             loss = np.min(history['loss'])
             acc = np.max(history['acc'])
             val_loss = np.min(history['val_loss'])
             val_acc = np.max(history['val_acc'])
-            line = ','.join(map(str,[learning_rate, loss, acc, val_loss, val_acc]))
+            line = ','.join(map(str,[lr, decay, loss, acc, val_loss, val_acc]))
             print(line)
             f.write(line)
             f.write('\n')
             f.flush()
 
-def learner():
-    model = make_model()
-    history = run(model, epoch_lengths, learning_rate, batches, test_batches)
-    pandas.DataFrame(history).to_csv("history.csv")
+def learner(settings, batches, test_batches, X_train):
+    history = run(settings, batches, test_batches, X_train)
+    pandas.DataFrame(history).to_csv("history-{}-{}-{}.csv".format(settings.epoch_lengths[0], settings.learning_rates[0], decay))
     model.save('final_model.h5')
 
+def run_our_model():
+    settings = Settings(batch_size = 32,
+            epoch_lengths = [100],
+            learning_rates = [0.015],
+            decay = 3e-5,
+            input_dropout = False,
+            orthonormal_init = True)
+    X_train, Y_train, X_test, Y_test = load_data()
+    batches, datagen = preprocess_dataset(X_train, Y_train, settings)
+    test_batches = (X_test, Y_test)
+    learner(settings, batches, test_batches, X_train)
 
-K.set_image_dim_ordering('tf')
+def run_their_model():
+    settings = Settings(batch_size = 64,
+            epoch_lengths = [200, 50, 50, 50],
+            learning_rates = [0.05, 0.005, 0.0005, 0.00005],
+            decay = 0.001,
+            input_dropout = True,
+            orthonormal_init = False)
+    X_train, Y_train, X_test, Y_test = load_data()
+    batches, datagen = preprocess_dataset(X_train, Y_train, settings)
+    test_batches = (X_test, Y_test)
+    learner(settings, batches, test_batches, X_train)
 
-batch_size = 64
-epochs = 350
-epochs = 1
-rows, cols = 32, 32
-channels = 3
-#learning_rate = [0.01, 0.001, 0.0001, 0.00001]
-#epoch_lengths = [200, 50, 50, 50]
-learning_rate = [0.01]
-epoch_lengths = [100]
-
-path="weights.hdf5"
-
-X_train, Y_train, X_test, Y_test = load_data()
-#whitener = preprocess_data(X_train)
-#preprocess_data(X_test, whitener)
-
-batches, datagen = preprocess_dataset(X_train, Y_train, batch_size)
-test_batches = datagen.flow(X_test, Y_test, batch_size = len(X_test[0]))
-
-#for i in range(len(X_test)):
-#    X_test[i] = datagen.standardize(X_test[i])
-
-for a, b in zip([X_train, Y_train, X_test, Y_test], ['X','y','X_test','y_test']):
-    print('{} shape : {}'.format(b, a.shape))
-
-#param_search([1e-2, 1e-3, 1e-4, 1e-5, 1e-6], [0.012, 0.012, 0.012, 0.012, 0.012])
-learner()
-
-#def resize(infile):
-#    from PIL import Image
-#    im = Image.open(infile)
-#    im = im.resize((224, 224), Image.ANTIALIAS)
-#    return np.array(im).astype(np.float32)
-#
-#im = resize('image.png')
-#out = model.predict(im)
-#print(np.argmax(out))
+if __name__ == "__main__":
+    run_our_model()
+    #param_search([3e-5, 3e-5, 3e-5, 3e-5, 3e-5, 3e-5], [0.005, 0.01, 0.015, 0.02, 0.025, 0.03], 20)
